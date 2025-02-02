3e8e0e8d69f9c132d709cc9219e6f004e8459239
pacman <oiquetoi199@gmail.com>
2025-02-02 15:54:47 +0700

    commit 2

--- src/main/java/com/project1/myproject/MyprojectApplication.java
+++ src/main/java/com/project1/myproject/MyprojectApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @SpringBootApplication(exclude =
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+// @SpringBootApplication(exclude = {
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+// org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+// })
 public class MyprojectApplication {
 
 	public static void main(String[] args) {
